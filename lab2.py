a25449c28a9d77ea0973bfee8853f58dc9b6197a
boichuk10 <90177837+boichuk10@users.noreply.github.com>
2023-04-11 14:28:56 +0300

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,17 @@
+#PR-2 Boichuk Juliia
+def prime_num_generator():
+    n = 2
+    while True:
+        prime = True
+
+        if prime:
+            yield n
+
+        for i in range(2, int(n//2)+1):
+            if n % 1 == 0:
+                prime = False
+                break
+
+        n +=1
+
+#print(prime_num_generator())
